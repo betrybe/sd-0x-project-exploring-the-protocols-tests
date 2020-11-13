9d1b1e5a02867200c215ec5e48324db2fd9c692c
“brunobatista” <brunobatista101@gmail.com>
2020-11-13 16:02:42 -0300

    teste

--- tests/server1.spec.js
+++ tests/server1.spec.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 var execTerminal = require('child_process').exec, child;
+var spawn = require('child_process').spawn;
 
 const BASE_URL = 'http://localhost:4040/inspect/http';
 
@@ -32,36 +33,48 @@ describe('Responder o IP do client', () => {
     let page;
     const instructions = fs.readFileSync('./instruction.json', 'utf8');
     const instructionsString = JSON.parse(instructions.toString());
-    let execToken = execTerminal(`./ngrok authtoken ${instructionsString.token}`);
-    let execNgrok = execTerminal('./ngrok http 8080');
-    let execNode = execTerminal('node src/index.js');
+   // let execToken = execTerminal(`./ngrok authtoken ${instructionsString.token}`);
+   // let execNgrok = execTerminal('./ngrok http 8080');
+   // let execNode = execTerminal('node src/index.js');
+
+
+
+var execToken = spawn(`./ngrok authtoken ${instructionsString.token}`, {detached: true});
+var execNgrok = spawn('./ngrok http 8080', {detached: true});
+var execNode = spawn('node src/index.js', {detached: true});
+
+process.kill(-child.pid);
     beforeEach(async () => {
       browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--window-size=1920,1080'], headless: true });
       page = await browser.newPage();
     });
   
     afterEach(async () => {
-      execToken.kill();
-      sleep(10000)
-      execNgrok.kill();
-      sleep(10000)
-      execNode.kill();
-      sleep(10000)
+        process.kill(-execToken.pid);
+      //execToken.kill();
+      sleep(20000)
+
+      process.kill(-execNode.pid);
+      //execNode.kill();
+      sleep(20000)
+      process.kill(-execNgrok.pid);
+      //execNgrok.kill();
+      sleep(20000)
     });
   
     it('Será validado que ao acessar a url sera possível visualizar o ip do client', async () => {
   
       
       execToken.stdout.on('data', ()=>{ });
-      sleep(10000)
+      sleep(20000)
       
       execNgrok.stdout.on('data', ()=>{ });
-      sleep(10000)
+      sleep(20000)
       
       execNode.stdout.on('data', ()=>{ });
-      sleep(10000)
+      sleep(20000)
       await page.goto(BASE_URL);
-      sleep(10000)
+      sleep(20000)
       await page.waitForSelector('a[target="_blank"]');
       const url =  await page.$$eval('a[target="_blank"]', (nodes) => nodes.map((n) => n.innerText));
       console.log(url)
@@ -70,7 +83,7 @@ describe('Responder o IP do client', () => {
       console.log(url[1]);
       newPage.goto(url[1]);
       console.log(url[1]);
-      sleep(10000)
+      sleep(20000)
       await newPage.waitForSelector(dataTestid('ip'));
   
       const textIp = await newPage.$$eval(dataTestid('ip'), (nodes) => nodes.map((n) => n.innerText));
--- tests/server2.spec.js
+++ tests/server2.spec.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 var execTerminal = require('child_process').exec, child;
+var spawn = require('child_process').spawn;
 
 const BASE_URL = 'http://localhost:4040/inspect/http';
 
@@ -32,9 +33,13 @@ function wait(time) {
     let page;
     const instructions = fs.readFileSync('./instruction.json', 'utf8');
     const instructionsString = JSON.parse(instructions.toString());
-    var execToken = execTerminal(`./ngrok authtoken ${instructionsString.token}`);
-    var execNgrok = execTerminal('./ngrok http 8080');
-    var execNode = execTerminal('node src/index.js');
+    //var execToken = execTerminal(`./ngrok authtoken ${instructionsString.token}`);
+    //var execNgrok = execTerminal('./ngrok http 8080');
+    //var execNode = execTerminal('node src/index.js');
+
+    var execToken = spawn(`./ngrok authtoken ${instructionsString.token}`, {detached: true});
+var execNgrok = spawn('./ngrok http 8080', {detached: true});
+var execNode = spawn('node src/index.js', {detached: true});
   
     beforeEach(async () => {
       browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--window-size=1920,1080'], headless: true });
@@ -42,36 +47,40 @@ function wait(time) {
     });
   
     afterEach(async () => {
-      execToken.kill();
-      sleep(10000)
-      execNgrok.kill();
-      sleep(10000)
-      execNode.kill();
-      sleep(10000)
+        process.kill(-execToken.pid);
+        //execToken.kill();
+        sleep(20000)
+  
+        process.kill(-execNode.pid);
+        //execNode.kill();
+        sleep(20000)
+        process.kill(-execNgrok.pid);
+        //execNgrok.kill();
+        sleep(20000)
     });
   
     it('Será validado que as informações da localização do cliente serão exibidas na tela', async () => {
   
   
       execToken.stdout.on('data', ()=>{ });
-      sleep(10000)
+      sleep(20000)
       
       execNgrok.stdout.on('data', ()=>{ });
-      sleep(10000)
+      sleep(20000)
    
       execNode.stdout.on('data', ()=>{ });
-      sleep(10000)
+      sleep(20000)
       await page.goto(BASE_URL);
-      sleep(10000)
+      sleep(20000)
       await page.waitForSelector('a[target="_blank"]');
       const url =  await page.$$eval('a[target="_blank"]', (nodes) => nodes.map((n) => n.innerText));
       console.log(url)
       newPage = await browser.newPage();
-      sleep(10000)
+      sleep(20000)
       console.log(url[1]);
       newPage.goto(url[1]);
       console.log(url[1]);
-      sleep(10000)
+      sleep(20000)
       await newPage.waitForSelector(dataTestid('city'));
       const textCity = await newPage.$$eval(dataTestid('city'), (nodes) => nodes.map((n) => n.innerText));
       await newPage.waitForSelector(dataTestid('postal_code'));
