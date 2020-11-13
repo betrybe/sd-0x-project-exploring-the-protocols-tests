a08348587c9faa17d9f559b10ce753fe0ba68be6
“brunobatista” <brunobatista101@gmail.com>
2020-11-13 16:52:48 -0300

    teste

--- tests/server4.spec.js
+++ tests/server4.spec.js
@@ -32,14 +32,14 @@ describe('Responder a request com os resources do Server', () => {
     let page;
     const instructions = fs.readFileSync('./instruction.json', 'utf8');
     const instructionsString = JSON.parse(instructions.toString());
-     
+    let url2 = '';
 
 
     var execNode = execTerminal('node src/index.js');
 
     beforeEach(async () => {
-        await ngrok.authtoken(instructionsString.token);
-        const url2 = await ngrok.connect(8080);
+      await ngrok.authtoken(instructionsString.token);
+      url2 = await ngrok.connect(8080);
       browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage', '--window-size=1920,1080'], headless: true });
       page = await browser.newPage();
     });
